0133acb67d4ae0555945a04a59c237f586799651
fearofvoid <wardennkoil@gmail.com>
2025-02-17 11:44:29 -0500

    wip analysis-landing

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import LandingPage from "./views/LandingPage/LandingPage.vue";
+</script>
+
+<template>
+  <div class="w-full h-full">
+    <LandingPage/>
+  </div>
+</template>
+
+<style>
+</style>
