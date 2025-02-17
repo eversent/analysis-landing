03a37e326a91d0dd89960a06976cea1889d26e10
fearofvoid <wardennkoil@gmail.com>
2025-02-17 11:38:39 -0500

    wip analysis-landing

---
+++ src/views/LandingPage/LandingPage.vue
@@ -0,0 +1,21 @@
+<script>
+import HeroSection from "../../components/LandingComponents/HeroSection.vue";
+
+export default {
+  name: "LandingPage",
+  components: {
+    HeroSection,
+  }
+}
+</script>
+
+<template>
+  <div class="bg-dark-def w-full h-full">
+    <h1>Hello!</h1>
+    <HeroSection></HeroSection>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
